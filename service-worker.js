65902e961fe3530005ea586f9a55005180901f41
Dean <deanhume@gmail.com>
2016-10-14 09:16:06 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
   'use strict';
 
   importScripts('./imagebeast.min.js');
-  optimize({ useWebp: true, useXr: true, useSaveData: true });
+  optimize({ useWebp: true, useXr: true, useSaveData: true, useCache: true });
 
   // Ensure that our service worker takes control of the page as soon as possible.
   global.addEventListener('install', event => event.waitUntil(global.skipWaiting()));
